d4c3c2d8df4c41b9f49b4e66fb460195c44a90a4
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-02-25 00:06:19 +0330

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const ExpressEjsLayouts = require("express-ejs-layouts");
+const { AllRoutes } = require("./router/router");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+require("dotenv").config()
+const {PORT} = process.env;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.set("view engin", "ejs");
+app.use(ExpressEjsLayouts);
+app.set("views", "views");
+app.set("layout", "./layouts/master");
+app.use(AllRoutes)
+app.use((req, res, next) => {
+    return res.status(404).json({
+        statusCode : 404,
+        message : "Not Found"
+    })
+});
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        statusCode : err.status || 500,
+        message : err.message
+    })
+});
+
+server.listen(PORT, () => {
+    console.log(`run > http://localhost:${PORT}`);
+})
