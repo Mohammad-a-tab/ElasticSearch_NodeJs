217d8b0fe7328a15aa87e47435af6f0887df347d
Mohammad-a-tab <mohammad.programmer.tab@gmail.com>
2023-02-26 21:48:04 +0330

    .

---
+++ config/elastic.config.js
@@ -0,0 +1,14 @@
+const { ELASTIC_USERNAME, ELASTIC_PASSWORD, ELASTIC_HOST } = process.env;
+const { Client } = require("@elastic/elasticsearch");
+
+const elasticClient = new Client({
+    node : ELASTIC_HOST,
+    auth : {
+        username : ELASTIC_USERNAME,
+        password : ELASTIC_PASSWORD 
+    }
+});
+
+module.exports = {
+    elasticClient
+}
---
+++ controller/blog.controller.js
@@ -0,0 +1,47 @@
+class BlogController {
+    async getAllBlogs (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async createNewBlog (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeBlog (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async searchByTitle (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async searchByMultiField (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async searchByRegexp (req, res, next) {
+        try {
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+module.exports = {
+    BlogController : new BlogController()
+}
